e69147581df2e178a07eff0e784fda7e64a3f625
strwbrry05 <jq.strwbrry05@gmail.com>
2025-06-05 19:58:46 -0700

    base QA complete

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,11 +2,42 @@ import React, { useState } from "react";
 import { LuCirclePlus } from "react-icons/lu";
 
 const Faq = () => {
-  const [isActive, setIsActive] = useState(true);
+  const [faq, setFaq] = useState([
+    {
+      question: "How to get around Tokyo? (Mode of Transport?)",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit.Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis.",
+      active: true,
+    },
+    {
+      question: "Packing essentials?",
+      answer:
+        "Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. ",
+      active: false,
+    },
+    {
+      question: "When to buy flights/accommodations?",
+      answer:
+        "In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. ",
+      active: false,
+    },
+    {
+      question: "Do I need to know Japanese to go to Japan?",
+      answer:
+        "Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. ",
+      active: false,
+    },
+  ]);
 
-  const openAnswer = () => {
-    console.log(isActive);
-    setIsActive(!isActive);
+  const toggleFAQ = (index) => {
+    setFaq(
+      faq.map((qa, i) => {
+        if (i === index) {
+          qa.active = !qa.active;
+        }
+        return qa;
+      })
+    );
   };
 
   return (
@@ -18,89 +49,41 @@ const Faq = () => {
         <h2 className="text-center text-[1.5rem] mb-[1em]">
           Frequently Asked Q's
         </h2>
-        <div className="flex flex-col justify-center items-start pb-[0.5em] ">
-          <div
-            className="z-1 flex justify-between items-center pb-[1em] border-b-2 w-full
-          bg-(--color-red)"
-          >
-            <p className="text-[1rem] ">
-              How to get around Tokyo? (Mode of Transport?)
-            </p>
-            <LuCirclePlus
-              className="text-[2.75rem] ml-[0.5em]"
-              onClick={openAnswer}
-            />
-          </div>
 
-          <div
-            className={` 
-            ${
-              isActive
-                ? "translate-y-[0vh] transition duration-500 ease-in "
-                : "translate-y-[-80px] transition duration-1000 ease-in "
-            }`}
-          >
-            <p
-              className={`z-0 font-(family-name:--font-text) text-[0.75rem] p-[1em] border-b-2 
-            ${
-              isActive
-                ? "transition duration-500 ease-in-out"
-                : "transition duration-500 ease-in-out"
-            }`}
+        {faq.map((qa, index) => {
+          return (
+            <div
+              key={index}
+              className="flex flex-col justify-center items-start pb-[0.5em]"
             >
-              Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
-              faucibus ex sapien vitae pellentesque sem placerat. In id cursus
-              mi pretium tellus duis convallis. Tempus leo eu aenean sed diam
-              urna tempor.
-            </p>
-          </div>
-        </div>
+              <div
+                className="z-1 flex justify-between items-center pb-[0.75em] border-b-2 mt-[0.5em] w-full 
+          bg-(--color-red)"
+                onClick={() => toggleFAQ(index)}
+              >
+                <p className="">{qa.question}</p>
+                {qa.active ? (
+                  <p className="text-[2.5rem]">-</p>
+                ) : (
+                  <p className="text-[2.25rem]">+</p>
+                )}
+              </div>
 
-        <div className="flex flex-col justify-center items-start pb-[0.5em] border-b-2">
-          <div className="flex justify-between items-center pb-[1em] border-b-2 mt-[1em] w-full">
-            <p className="text-[1rem] mr-[2em]">Packing essentials?</p>
-            <LuCirclePlus className="text-[1.5rem]" />
-          </div>
-
-          <p className="font-(family-name:--font-text) text-[0.75rem] p-[1em]">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
-            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
-            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
-            tempor.
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center items-start pb-[0.5em] border-b-2">
-          <div className="flex justify-between items-center pb-[1em] border-b-2 mt-[1em] w-full">
-            <p className="text-[1rem] mr-[2em]">
-              When to buy flights/accomadations?
-            </p>
-            <LuCirclePlus className="text-[2rem]" />
-          </div>
-
-          <p className="font-(family-name:--font-text) text-[0.75rem] p-[1em]">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
-            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
-            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
-            tempor.
-          </p>
-        </div>
-
-        <div className="flex flex-col justify-center items-start pb-[0.5em] border-b-2">
-          <div className="flex justify-between items-center pb-[1em] border-b-2 mt-[1em] w-full">
-            <p className="text-[1rem] mr-[3em]">
-              Do I need to know Japanese to go to Japan?
-            </p>
-            <LuCirclePlus className="text-[2.5rem]" />
-          </div>
-
-          <p className="font-(family-name:--font-text) text-[0.75rem] p-[1em]">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque
-            faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi
-            pretium tellus duis convallis. Tempus leo eu aenean sed diam urna
-            tempor.
-          </p>
-        </div>
+              <div
+                className={` 
+            ${
+              qa.active
+                ? "max-h-[1000px] opacity-100 transition-all duration-500 ease-in-out "
+                : "max-h-[0px] opacity-0 overflow-hidden transform transition-all duration-300 ease-in-out"
+            }`}
+              >
+                <p className="font-(family-name:--font-text) text-[1rem] p-[1em] border-b-2">
+                  {qa.answer}
+                </p>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
